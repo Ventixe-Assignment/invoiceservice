4c6d6a8ce282fcf3289a0353433418ecaf4911c4
Janne <tapioninetytwo@live.se>
2025-06-04 13:06:12 +0200

    Add project files.

---
+++ Presentaion/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+
+
+
+
+
+
+
+var app = builder.Build();
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.UseCors(p => { p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();});
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
