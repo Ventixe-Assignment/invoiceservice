6ffb0609374d2af152a3477bec9a77c33db2cdf2
Janne <tapioninetytwo@live.se>
2025-06-04 13:24:01 +0200

    Added two entities one for invoice and one for the issuer

---
+++ Presentaion/Data/Entities/InvoiceEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace Presentaion.Data.Entities;
+
+public class InvoiceEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string EventId { get; set; } = null!;
+    public string UserId { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string Status { get; set; } = "Pending";
+    public ICollection<InvoiceIssuerEntity> Issuers { get; set; } = [];
+}
---
+++ Presentaion/Data/Entities/InvoiceIssuerEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Presentaion.Data.Entities;
+
+public class InvoiceIssuerEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [ForeignKey(nameof(Invoice))]
+    public string InvoiceId { get; set; } = null!;
+    public InvoiceEntity? Invoice { get; set; }
+
+}
