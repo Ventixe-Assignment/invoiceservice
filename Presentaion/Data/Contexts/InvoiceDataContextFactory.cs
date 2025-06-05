092d654bb6121fba5af4c552d2b4a8d2625e8c45
Janne <tapioninetytwo@live.se>
2025-06-05 17:38:22 +0200

    Have to add the factory to create tables, forgot that

---
+++ Presentaion/Data/Contexts/InvoiceDataContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Presentaion.Data.Contexts;
+
+public class InvoiceDataContextFactory : IDesignTimeDbContextFactory<InvoiceDataContext>
+{
+    public InvoiceDataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<InvoiceDataContext>();
+
+        var connectionString = Environment.GetEnvironmentVariable("SqlConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new InvoiceDataContext(optionsBuilder.Options);
+    }
+
+
+}
